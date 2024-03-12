dd558d16d225f451de3db45d9a6d39c3ec3958e9
raihan-jishan <raihanjishan3242@gmail.com>
2024-03-12 22:52:12 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Routes from "./Routes/Routes.jsx";
+import BottomSlider from "./components/BottomSlider/BottomSlider.jsx";
+import Nav from "./components/Nav/Nav.jsx";
 import { AppContext } from "./context/newsContext.jsx";
-import Layout from "./layout/Layout.jsx";
-function App() { 
+function App() {
   return (
     <AppContext>
       <div className="font-Montserrat bg-white dark:bg-zinc-900 dark:text-zinc-50">
-        <Layout />
+        <div className="sticky top-0">
+          <Nav />
+          <BottomSlider />
+          </div>
+          <Routes />
+        
       </div>
     </AppContext>
   );
